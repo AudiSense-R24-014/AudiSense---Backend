aa23bac6795e4a3a198a6570c6dabc7c86b01920
Randula Mawaththta <randula98@gmail.com>
2024-04-02 21:52:26 +0530

    Swagger URL log added

--- index.js
+++ index.js
@@ -30,4 +30,5 @@ app.get('/', (req, res) => {
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000')
+    console.log('Swagger is running on http://localhost:3000/api-docs')
 })
