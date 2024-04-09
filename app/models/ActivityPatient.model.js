b4660203e92790e9c9977e0c06b3cf5f811cccd5
Randula Mawaththta <randula98@gmail.com>
2024-04-09 22:02:19 +0530

    ActivityPatient Model Fixed
    Signed-off-by: Randula Mawaththta <randula98@gmail.com>

--- app/models/ActivityPatient.model.js
+++ app/models/ActivityPatient.model.js
@@ -1,7 +1,7 @@
 class ActivityPatient {
     constructor(id, activity_id, patient_id, status) {
         this.id = id;
-        this.activity_id = activity
+        this.activity_id = activity_id
         this.patient_id = patient_id
         this.status = status
     }
